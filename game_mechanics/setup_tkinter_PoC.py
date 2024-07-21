7330e060badcf269f9f82a41ea2b722efb0a9d60
suolenkainen <pmarjama@gmail.com>
2024-07-21 19:27:16 +0300

    Proof on concept that Interface can be made

--- game_mechanics/setup_tkinter_PoC.py
+++ game_mechanics/setup_tkinter_PoC.py
@@ -7,6 +7,7 @@ This also creates the UI for following card values in stock market.
 """
 
 from tkinter import *
+from tkinter import font
 import setup_cards as cards
 
 # Check that cards are present
@@ -40,36 +41,51 @@ def create_card_to_UI(mock_hand):
         end_x =  140 + card * 120
         end_y = 460
 
+        rect_coords = start_x, start_y, end_x, end_y
+        cost_coords = start_x-5, start_y+5, start_x+5, start_y+15
+        text_coords = start_x+5, start_y+17
+
         # Draw the rectangles to the canvas
-        canvas.create_rectangle(start_x, start_y, end_x, end_y, fill="white")
+        canvas.create_rectangle(rect_coords, fill="white")
 
         # Add card cost and name to cards
-        canvas.create_text(start_x+5, start_y+5, text=hand_card.cost, font="Arial 9", anchor="nw")
-        canvas.create_text(start_x+5, start_y+17, text=hand_card.name, font="Arial 9", anchor="nw")
+        create_icons_for_cost(hand_card.cost, cost_coords)
+
+        # canvas.create_text(cost_coords, text=hand_card.cost, font="Arial 9", anchor="nw")
+        input_text = align_text_and_row(hand_card.name)
+        canvas.create_text(text_coords, text=input_text, font="Arial 9", anchor="nw")
         card += 1
     
     # Create an image for the deck
-    canvas.create_rectangle(start_x + 140, start_y, end_x + 140, end_y, fill="light blue")
-    canvas.create_rectangle(start_x + 143, start_y + 3, end_x + 143, end_y + 3, fill="light blue")
-    canvas.create_rectangle(start_x + 146, start_y + 6, end_x + 146, end_y + 6, fill="light blue")
-    canvas.create_rectangle(start_x + 149, start_y + 9, end_x + 149, end_y + 9, fill="light blue")
+    for x in range(5):
+        canvas.create_rectangle(start_x + 140 + 3*x, start_y + 3*x, end_x + 140 + 3*x, end_y + 3*x, fill="light blue")
     canvas.create_text(start_x + 154, start_y+14, text="Draw deck", font="Arial 9", anchor="nw")
+
+
+def create_icons_for_cost(cost, coords):
+    if cost:
+        if isinstance(cost, str): cost = [cost]
+        for icon in cost:
+            coords = coords[0] + 12, coords[1], coords[2] + 12, coords[3]
+            if icon == "any":
+                icon = "grey"
+            canvas.create_oval(coords, fill=icon)
+
+
+def align_text_and_row(text, front_text = ""):
+    max_width = 90
+    text_object = font.Font(family="Arial", size=9)
+    width_in_pixels = text_object.measure(text)
+    print(width_in_pixels)
+    if width_in_pixels > max_width:
+        text = text.replace(" ", "\n")
+    print(text)
+    return text
+
 
 create_card_to_UI(mock_hand)
 
 # Run UI
 root.mainloop()
 
-
-
-# Add items
-# Add widgets??
-
-# Layout
-
-# 
-
-# Fetch data
-
-# Draw pictures
 
